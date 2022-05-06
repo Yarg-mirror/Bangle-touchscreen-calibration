68d08fcfbdcfc5d473fe8f3435e2668c98cdb2f3
Kane <kane@yarg.fr>
2022-05-06 14:34:05 +0200

    app.js: Object! Object everywhere! todo: explain() function.
    boot.js:
    reading from the flash at each input is bad, fixed.

--- app.js
+++ app.js
@@ -1,64 +1,85 @@
-app = {};
-
-function init() {
-  E.srand(Date.now());
-  g.setFont('Vector', 10);
-	app.appName = 'calibration';
-  app.xoffset = 0;
-  app.yoffset = 0;
-	app.x = 0;
-	app.y = 0;
-
-  let modes = {
-    idx   : 0,
-    mode  : 'custom',
-    btn   : function(n) {
-      save();
-      Bangle.showLauncher();
-    },
-    touch : function(btn, xy) {
-      app.xoffset = Math.round((app.xoffset + (app.x - Math.floor((app.x + xy.x)/2)))/2);
-      app.yoffset = Math.round((app.yoffset + (app.y - Math.floor((app.y + xy.y)/2)))/2);
-      drawTarget();
-    },
-  };
-  Bangle.setUI(modes);
+class BanglejsApp {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.settings = {
+      xoffset: 0,
+      yoffset: 0,
+      };
   }
 
-function explain() {
-  /*
-   * TODO:
-   * Present how to use the application
-   *
-   */
+  load_settings() {
+    let settings = require('Storage').readJSON('calibration.json', true) || {active: false};
+
+    // do nothing if the calibration is deactivated
+    if (settings.active === true) {
+      // cancel the calibration offset 
+      Bangle.on('touch', function(button, xy) {
+        xy.x += settings.xoffset;
+        xy.y += settings.yoffset;
+      });
+    }
+    if (!settings.xoffset) settings.xoffset = 0;
+    if (!settings.yoffset) settings.yoffset = 0;
+
+    console.log('loaded settings:');
+    console.log(settings);
+
+    return settings;
+  }
+
+  save_settings() {
+    this.settings.active = true;
+    this.settings.reload = false;
+    require('Storage').writeJSON('calibration.json', this.settings);
+
+    console.log('saved settings:');
+    console.log(this.settings);
+  }
+
+  explain() {
+    /*
+    * TODO:
+    * Present how to use the application
+    *
+    */
+  }
+
+  drawTarget() {
+    this.x = 16 + Math.floor(Math.random() * (g.getWidth() - 32));
+    this.y = 40 + Math.floor(Math.random() * (g.getHeight() - 80));
+
+    g.clearRect(0, 24, g.getWidth(), g.getHeight() - 24);
+    g.drawLine(this.x, this.y - 5, this.x, this.y + 5);
+    g.drawLine(this.x - 5, this.y, this.x + 5, this.y);
+    g.setFont('Vector', 10);
+    g.drawString('current offset: ' + this.settings.xoffset + ', ' + this.settings.yoffset, 0, 24);
+  }
+
+  setOffset(xy) {
+    this.settings.xoffset = Math.round((this.settings.xoffset + (this.x - Math.floor((this.x + xy.x)/2)))/2);
+    this.settings.yoffset = Math.round((this.settings.yoffset + (this.y - Math.floor((this.y + xy.y)/2)))/2);
+  }
 }
 
-function save() {
-  require('Storage').writeJSON(app.appName + '.json', {
-    xoffset: app.xoffset,
-    yoffset: app.yoffset,
-  });
-}
 
-function drawTarget() {
-  app.x = 16 + Math.floor(Math.random() * (g.getWidth() - 32));
-  app.y = 40 + Math.floor(Math.random() * (g.getHeight() - 80));
-
-  g.clearRect(0, 24, g.getWidth(), g.getHeight() - 24);
-  g.drawLine(app.x, app.y - 5, app.x, app.y + 5);
-  g.drawLine(app.x - 5, app.y, app.x + 5, app.y);
-  g.drawString('current offset: ' + app.xoffset + ', ' + app.yoffset, 0, 24);
-}
-
-
+E.srand(Date.now());
 Bangle.loadWidgets();
 Bangle.drawWidgets();
 
-explain();
+calibration = new BanglejsApp();
+calibration.load_settings();
 
-// if a calibration was previously made cancel it's effects
-save();
-
-init();
-
-drawTarget();
+let modes = {
+  mode  : 'custom',
+  btn   : function(n) {
+    calibration.save_settings(this.settings);
+    load();
+  },
+  touch : function(btn, xy) {
+    calibration.setOffset(xy);
+    calibration.drawTarget();
+  },
+};
+Bangle.setUI(modes);
+calibration.drawTarget();
